a354b6fd274bf27e2bb23ae9b4468ad2d807fe4b
deepspring17 <50515840+deepspring17@users.noreply.github.com>
2022-06-09 02:07:51 -0700

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -27,3 +27,33 @@ Centos7：对于Centos7系统来说，直接使用enable即可。
 
 #systemctl disable nginx.service 关闭开机自启
 
+
+yum install -y squid
+
+service squid start
+service squid restart
+
+chkconfig --add squid
+chkconfig squid on
+chkconfig --list
+
+visible_hostname yourhostname.com
+vim /etc/squid/squid.conf
+acl Safe_ports port 80
+acl guests src 43.134.209.194
+http_access allow guests
+
+
+curl -x 104.168.137.25:3128 http://httpbin.org/get
+tail -f /var/log/squid/access.log
+
+request_header_access X-Forwarded-For deny all  
+request_header_access From deny all  
+request_header_access Via deny all
+
+# ipv6 服务器本身也无法访问
+dns_v4_first on
+
+service iptables status
+service iptables stop
+
